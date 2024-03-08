585a9e7999857c4f5592728ed883998240de4882
marija <marija.pesha@gmail.com>
2024-03-08 13:56:41 +0100

    add HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import GetInTouch from "./pages/GetInTouch";
 
 function App() {
   return (
-    <Router basename="/marija-bajo">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/get-in-touch" element={<GetInTouch />} />
         <Route path="*" element={<div>No Match</div>} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
